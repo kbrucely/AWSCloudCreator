cda8cfc96509cd079b8a6514ff44e39aa4eb3340
Kevin Ly <kevinly630@gmail.com>
2019-10-27 19:51:46 -0400

    first commit

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "the aws region to run in"
+}
+
+variable "aws_access_key" {
+  descripition = "the access key of our aws user"
+}
+
+variable "aws_secret_key" {
+  description = "the secret key of our aws user"
+}
