94f46b545cb3a7a97ae366ad4c0af7dc01390399
Kevin Ly <kevinly630@gmail.com>
2019-10-27 22:22:20 -0400

    add public and private subnets to mgmt network

--- managementvpc.tf
+++ managementvpc.tf
@@ -5,3 +5,22 @@ resource "aws_vpc" "management" {
     Name = "management"
   }
 }
+
+resource "aws_subnet" "mgmt_public" {
+  vpc_id = "${aws_vpc.management.id}"
+  cidr_block = "${var.management_public_cidr}"
+  map_public_ip_on_launch = true                # set this because public subnet
+
+  tags = {
+    Name = "mgmt_public"
+  }
+}
+
+resource "aws_subnet" "mgmt_private" {
+  vpc_id = "${aws_vpc.management.id}"
+  cidr_block = "${var.management_private_cidr}"
+
+  tags = {
+    Name = "mgmt_private"
+  }
+}
