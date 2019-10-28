cf0417cdc90cd9d53cab7d3133fe166964fe4881
Kevin Ly <kevinly630@gmail.com>
2019-10-27 20:58:47 -0400

    removed skip validation, sts works on all accounts

--- provider.tf
+++ provider.tf
@@ -2,5 +2,4 @@ provider "aws" {
   region     = "${var.aws_region}"
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
-  skip_credentials_validation = true  #aws security token service will squash this for accounts without this implemented
 }
