7216eaa35be2fcee8e4b0f7a01a0f6406dc32f6d
Kevin Ly <kevinly630@gmail.com>
2019-10-27 20:40:15 -0400

    aws sts validation skip with notes

--- provider.tf
+++ provider.tf
@@ -2,5 +2,5 @@ provider "aws" {
   region     = "${var.aws_region}"
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
-  profile = "terraform"
+  skip_credentials_validation = true  #aws security token service will squash this for accounts without this implemented
 }
